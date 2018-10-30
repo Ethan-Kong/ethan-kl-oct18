dece6206779185d32f7692e613dde950b097d2ae
ethan.kong <ethan.kong@pslgroup.com>
2018-10-30 16:21:08 +0800

    String Check file

---
+++ stringcheck.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 12:01:57 2018
+
+@author: ethan.kong
+"""
+
+message = "Test message"
+
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
