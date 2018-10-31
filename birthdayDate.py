82eeb44efb7ae9bb0b45c1e0ded0f18f81b00de8
ethan.kong <ethan.kong@pslgroup.com>
2018-10-31 16:05:56 +0800

    Birthday date

--- birthdayDate.py
+++ birthdayDate.py
@@ -5,13 +5,6 @@ Created on Wed Oct 31 10:42:12 2018
 @author: ethan.kong
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 31 10:10:39 2018
-
-@author: ethan.kong
-"""
-
 import datetime;
 
 birthday = input("What is your birthday? (mm/dd/yy)");
