60f454d108710b49036cffe5f027a9a90b5425e4
ethan.kong <ethan.kong@pslgroup.com>
2018-11-01 10:36:26 +0800

    Turtle color

---
+++ turtle.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 10:05:30 2018
+
+@author: ethan.kong
+"""
+
+import turtle
+turtle.color('green')
+for step in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+turtle.color('red')    
+turtle.forward(200)
+    
