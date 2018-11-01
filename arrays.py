92d35637c821995297c51a7908a5bd16fa25e1d0
ethan.kong <ethan.kong@pslgroup.com>
2018-11-01 11:38:46 +0800

    Arrays

---
+++ arrays.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:34:00 2018
+
+@author: ethan.kong
+"""
+
+guests = ['first','second','third']
+print(guests[1])
+
+scores = [78,68,88,98,25]
+print(scores[3])
