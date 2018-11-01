4ec41ec81d6eb0d4e038cf318525159863b92013
ethan.kong <ethan.kong@pslgroup.com>
2018-11-01 11:20:12 +0800

    loop

--- loop.py
+++ loop.py
@@ -7,5 +7,7 @@ Created on Thu Nov  1 11:00:43 2018
 
 #for steps in range(4):
 #for steps in range(1,10,2):
-for steps in [1,2,3,4,5]:
-    print (steps)
+#for steps in [1,2,3,4,5]:
+#for steps in ['red','blue','green','black']:
+for steps in ['red','blue','green','black',8]: 
+   print (steps)
