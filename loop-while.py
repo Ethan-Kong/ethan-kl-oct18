076234621a8791c20e52523f6b172080de32ea9f
ethan.kong <ethan.kong@pslgroup.com>
2018-11-01 11:20:45 +0800

    loop-while

---
+++ loop-while.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:14:30 2018
+
+@author: ethan.kong
+"""
+
+answer = 0
+
+while answer !=4:
+    answer=float(input("What is 2+2? "))
+    
+print("Yes! 2+2 = 4")    
