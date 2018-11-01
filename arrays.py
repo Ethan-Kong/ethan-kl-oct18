35a6fa1b63fb47544a7feaa8a2242de324d5da87
ethan.kong <ethan.kong@pslgroup.com>
2018-11-01 11:57:03 +0800

    Arrays

--- arrays.py
+++ arrays.py
@@ -6,7 +6,30 @@ Created on Thu Nov  1 11:34:00 2018
 """
 
 guests = ['first','second','third']
-print(guests[1])
+#print(guests[1])
+#print(guests[-1])
+#print('First value is Initial : ' + guests[0])
+
+#guests[0] = 'Steve'
+
+#print('First value is Initial : ' + guests[0])
+
+#guests.append('New Guy')
+
+#print('New value is now : ' + guests[-1])
+
+#guests.remove('second')
+#print('Second Element is : ' + guests[1])
+
+#del guests[1]
+#print('Second Element after remove is : ' + guests[1])
+
+#print(guests.index('second'))
+
+#for step in range(4):
+for step in range(len(guests)):
+    print(guests[step])
 
 scores = [78,68,88,98,25]
-print(scores[3])
+#print(scores[3])
+#print(scores[-1])
