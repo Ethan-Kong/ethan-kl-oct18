30985de1ee0aa440a36ca1c0f9f47fb4fb9b85b8
ethan.kong <ethan.kong@pslgroup.com>
2018-11-05 11:07:24 +0800

    Function Check

---
+++ functionCheck.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 11:01:44 2018
+
+@author: ethan.kong
+"""
+
+#1. Create a function to print Hello
+
+def sayHello() :
+    print("Hello there!")
+    return
+
+#2. Involke the funtion
+sayHello()
