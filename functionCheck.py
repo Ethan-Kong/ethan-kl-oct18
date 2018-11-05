1408f6c254be9fa1cd20560ed7dc124081a5ca9b
ethan.kong <ethan.kong@pslgroup.com>
2018-11-05 11:20:00 +0800

    Function Check

--- functionCheck.py
+++ functionCheck.py
@@ -6,10 +6,34 @@ Created on Mon Nov  5 11:01:44 2018
 """
 
 #1. Create a function to print Hello
+"""
+def sayHello(name) :
+    #print("Hello there!")
+    print("Hello " + name + ", how are you?")
+    return
+"""
 
-def sayHello() :
-    print("Hello there!")
+def sayHello(firstname, lastname) :
+    #print("Hello there!")
+    print("Hello " + firstname + " " + lastname + " , how are you?")
     return
 
 #2. Involke the funtion
-sayHello()
+#sayHello("abc","123")
+#sayHello("456","")
+
+def addTwo(var1, var2) :
+    sum = var1 + var2
+    return sum
+
+#print("Sum of 5 + 5 = " + str(addTwo(5,5)))
+    
+def printInfo(name, age) :
+    print ("Name: ", name)
+    print ("Age: ", age)
+    return;
+
+""" 2 way of calling of function """
+printInfo("Elle",50)    
+printInfo(age=50,name="Elle")
+    
