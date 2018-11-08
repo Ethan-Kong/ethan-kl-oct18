ed44fd7eab5816c8e58561a3842a4cf21e49079e
ethan.kong <ethan.kong@pslgroup.com>
2018-11-08 11:06:52 +0800

    Critter

---
+++ critter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 10:56:59 2018
+
+@author: ethan.kong
+"""
+
+class Critter :
+    def __init__(self, msg) :        
+        self.msg = msg
+        
+    def talk(self) :
+      print(self.para)  
+
+critter1 = Critter("Hi!")
+critter1.talk()
