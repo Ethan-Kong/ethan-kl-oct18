7f20d53528a13adce073e8bc6ae45445c2264c81
ethan.kong <ethan.kong@pslgroup.com>
2018-11-14 10:38:33 +0800

    adder

---
+++ adder.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 10:36:31 2018
+
+@author: ethan.kong
+"""
+
+def add_item(item, item_list = []):
+    item_list.append(item)
+    print item_list
