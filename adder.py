574638f8781fddd249dde0cd4f41913b1d6fb71f
ethan.kong <ethan.kong@pslgroup.com>
2018-11-14 10:59:36 +0800

    adder

--- adder.py
+++ adder.py
@@ -4,7 +4,24 @@ Created on Wed Nov 14 10:36:31 2018
 
 @author: ethan.kong
 """
-
+"""
 def add_item(item, item_list = []):
     item_list.append(item)
     print (item_list)
+"""
+
+def add_item(item, item_list = None):
+    if item_list == None :
+        item_list = []
+        
+    item_list.append(item)
+    print (item_list)    
+    
+def printIt(**kwargs):
+    print(kwargs)    
+    
+def printDict(**kwargs):
+    print(kwargs)      
+    
+def printTuple(*args):
+    print(args)      
