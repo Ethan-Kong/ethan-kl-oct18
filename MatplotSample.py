698119ac64c2350dce3c98eeea9e8d60531c137d
ethan.kong <ethan.kong@pslgroup.com>
2018-11-15 11:06:11 +0800

    Matplot Sample

---
+++ MatplotSample.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 10:57:39 2018
+
+@author: ethan.kong
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.ylabel('Some Significant Number')
+plt.show()
